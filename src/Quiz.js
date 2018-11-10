afb0a443113e96db1b1164a8fd5f278b1e4ca099
Polly Banerjee <polly.banerjee.1957@gmail.com>
2018-11-11 01:29:20 +0530

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,7 +13,7 @@ class Quiz extends Component {
     render() {
         return (
             <div>
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position]} />
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
             </div>
         );
     }
