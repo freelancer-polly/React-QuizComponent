060a252727a3638bbe4e43c24657a98be094f0f2
Polly Banerjee <polly.banerjee.1957@gmail.com>
2018-11-12 01:22:27 +0530

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,9 @@ class Quiz extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {quiz_position: 1};
+        this.state = {
+            quiz_position: 1
+        };
     }
 
     showNextQuestion() {
